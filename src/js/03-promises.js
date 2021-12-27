77b5a0614ddc4d1c1565b3c87c0733a35d2ea128
AleksandrTatarenko <78167369+AleksandrTatarenko@users.noreply.github.com>
2021-12-27 22:25:52 +0200

    hw 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,13 +4,38 @@ const refs = {
   form: document.querySelector('.form'),
 }
 
-refs.form.addEventListener('submit', onBtnSubmitCreatePromise);
+refs.form.addEventListener('submit', submitPromise);
 
-let position = 0;
-
-function onBtnSubmitCreatePromise(e) {
+function submitPromise(e) {
   e.preventDefault();
-  const delay = Number(e.currentTarget.elements.delay.value);
+  const init = Number(e.currentTarget.elements.delay.value);
   const step = Number(e.currentTarget.elements.step.value);
   const amount = Number(e.currentTarget.elements.amount.value);
+  let position = 0;
+
+  setInterval(() => {
+    if (position === amount) { return; }
+    position += 1;
+    const delay = init + step * (position-1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  },1);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => { 
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(({position, delay}));
+  } else {
+        reject(({position, delay}));
+  }
+  },delay)
+  })
 }
